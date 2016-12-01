748128d6f0927f0a0ef9a476847902c8083c7bc5
José Iván López Gónzalez <joseivanlopez@gmail.com>
2016-12-01 21:41:19 +0000

    Improve tests

--- test/cfa/ntp_conf_test.rb
+++ test/cfa/ntp_conf_test.rb
@@ -2,15 +2,23 @@ require_relative "../test_helper"
 require "cfa/memory_file"
 require "cfa/ntp_conf"
 
-def ntp_disk_file
+def ntp_disk_content
   path = File.expand_path("../../fixtures/cfa/ntp.conf", __FILE__)
   File.read(path)
 end
 
-describe CFA::NtpConf do
-  subject(:ntp) { described_class.new(file_handler: ntp_file) }
+def ntp_file(content)
+  CFA::MemoryFile.new(content)
+end
 
-  let(:ntp_file) { CFA::MemoryFile.new(content) }
+def ntp_conf(file)
+  CFA::NtpConf.new(file_handler: file)
+end
+
+describe CFA::NtpConf do
+  subject(:ntp) { ntp_conf(file) }
+
+  let(:file) { ntp_file(content) }
 
   before do
     ntp.load
@@ -29,7 +37,7 @@ describe CFA::NtpConf do
   end
 
   describe "#records" do
-    let(:content) { ntp_disk_file }
+    let(:content) { ntp_disk_content }
 
     it "obtains the corrent amount of records" do
       expect(ntp.records.count).to eq(12)
@@ -39,36 +47,80 @@ describe CFA::NtpConf do
       expect(ntp.records).to be_a(CFA::NtpConf::RecordCollection)
     end
   end
+
+  describe "#save" do
+    let(:content) do
+      "server 0.pool.ntp.org\n" \
+      "server 1.pool.ntp.org\n" \
+      "server 2.pool.ntp.org\n"
+    end
+
+    context "when a record is added" do
+      it "writes a new entry" do
+        record = CFA::NtpConf::ServerRecord.new
+        record.value = "3.pool.ntp.org"
+        ntp.records << record
+        ntp.save
+        expect(file.content.lines).to include("server 3.pool.ntp.org\n")
+      end
+    end
+
+    context "when a record is deleted" do
+      it "removes an entry" do
+        record = ntp.records.find { |r| r.value == "0.pool.ntp.org" }
+        ntp.records.delete(record)
+        ntp.save
+        expect(file.content.lines).not_to include("server 0.pool.ntp.org\n")
+      end
+    end
+
+    context "when a record is updated" do
+      it "modifies an entry" do
+        record = ntp.records.find { |r| r.value == "0.pool.ntp.org" }
+        record.value = "10.pool.ntp.org"
+        ntp.save
+        expect(file.content.lines).not_to include("server 0.pool.ntp.org\n")
+        expect(file.content.lines).to include("server 10.pool.ntp.org\n")
+      end
+    end
+  end
 end
 
 describe CFA::NtpConf::RecordCollection do
-  subject(:ntp) { CFA::NtpConf.new(file_handler: ntp_file) }
+  let(:ntp) { ntp_conf(file) }
 
-  let(:ntp_file) { CFA::MemoryFile.new(ntp_disk_file) }
+  let(:file) { ntp_file(content) }
 
   let(:new_record) do
     record = CFA::NtpConf::ServerRecord.new
-    record.value = "4.pool.ntp.org"
+    record.value = "3.pool.ntp.org"
     record
   end
 
-  let(:existing_record) { ntp.records.first.dup }
+  let(:existing_record) { ntp.records.first }
+
+  let(:content) do
+    "server 0.pool.ntp.org\n" \
+    "server 1.pool.ntp.org\n" \
+    "server 2.pool.ntp.org\n"
+  end
 
   before do
     ntp.load
   end
 
-  context "#add" do
+  context "#<<" do
     context "when does not exist the record to add" do
       it "adds the record" do
-        ntp.records.add(new_record)
+        ntp.records << new_record
         expect(ntp.records.include?(new_record)).to be(true)
+        expect(ntp.records.count(new_record)).to eq(1)
       end
     end
 
     context "when exists the record to add" do
       it "adds the record too" do
-        ntp.records.add(existing_record)
+        ntp.records << existing_record
         expect(ntp.records.count(existing_record)).to eq(2)
       end
     end
@@ -100,26 +152,30 @@ describe CFA::NtpConf::RecordCollection do
 end
 
 describe CFA::NtpConf::Record do
-  let(:augeas_options) do
-    tree = CFA::AugeasTree.new
-    tree.add("iburst", nil)
-    tree
+  let(:ntp) { ntp_conf(file) }
+
+  let(:file) { ntp_file(content) }
+
+  let(:content) do
+    "server 0.pool.ntp.org iburst\n" \
+    "server 1.pool.ntp.org #sample comment\n" \
+    "server 2.pool.ntp.org\n"
   end
 
-  let(:augeas_tree_value) do
-    CFA::AugeasTreeValue.new(augeas_options, "4.pool.ntp.org")
+  before do
+    ntp.load
   end
-
-  let(:augeas_element) do
-    {
-      key:   "server[]",
-      value: augeas_tree_value
-    }
-  end
-
-  subject(:record) { described_class.new_from_augeas(augeas_element) }
 
   describe ".new_from_augeas" do
+    let(:augeas_element) do
+      {
+        key:   "server[]",
+        value: "3.pool.ntp.org"
+      }
+    end
+
+    subject(:record) { described_class.new_from_augeas(augeas_element) }
+
     it "creates a record of the correct class" do
       expect(record).to be_a(CFA::NtpConf::ServerRecord)
     end
@@ -131,88 +187,98 @@ describe CFA::NtpConf::Record do
 
   describe "#value" do
     it "obtains the value of the record" do
-      expect(record.value).to eq(augeas_element[:value].value)
+      record = ntp.records.first
+      expect(record.value).to eq("0.pool.ntp.org")
     end
   end
 
   describe "#value=" do
     it "sets the value of the record" do
-      value = "1.pool.ntp.org"
+      value = "3.pool.ntp.org"
+      record = ntp.records.first
       record.value = value
       expect(record.value).to eq(value)
+      ntp.save
+      expect(file.content).to include("server 3.pool.ntp.org iburst\n")
     end
   end
 
   describe "#comment" do
     context "when the record has not comment" do
       it "obtains nil" do
+        record = ntp.records.first
         expect(record.comment).to eq(nil)
       end
     end
 
     context "when the record has comment" do
-      let(:augeas_options) do
-        tree = CFA::AugeasTree.new
-        tree.add("#comment", "sample comment")
-        tree
-      end
-
       it "obtains the comment" do
-        expect(record.comment).to eq("sample comment")
+        record = ntp.records.to_a[1]
+        expect(record.comment).to eq("#sample comment")
       end
     end
   end
 
   describe "#comment=" do
     it "sets a comment to the record" do
-      comment = "sample comment"
+      comment = "#sample comment"
+      record = ntp.records.first
       record.comment = comment
       expect(record.comment).to eq(comment)
+      ntp.save
+      expect(file.content).to include("server 0.pool.ntp.org iburst#sample comment\n")
     end
   end
 
   describe "#==" do
     it "returns true for equal records" do
-      equal_record = described_class.new_from_augeas(augeas_element)
-      expect(record == equal_record).to be(true)
+      record = ntp.records.first
+      expect(record == record.dup).to be(true)
     end
 
     it "returns false for different records" do
-      different_record = CFA::NtpConf::Record.new(key: "server[]")
-      different_record.value = "10.10.10.10"
-      expect(record == different_record).to be(false)
+      records = ntp.records.to_a
+      expect(records[0] == records[1]).to be(false)
     end
   end
 
   describe "#type" do
     it "obtains the type of the record" do
-      expect(record.type).to eq("server")
+      expect(ntp.records.first.type).to eq("server")
     end
   end
 
   describe "#raw_options" do
     it "obtains options as string" do
-      expect(record.raw_options).to eq("iburst")
+      expect(ntp.records.first.raw_options).to eq("iburst")
     end
   end
 
   describe "#raw_options=" do
     it "sets options from a string" do
+      record = ntp.records.first
       record.raw_options = "iburst prefer"
       expect(record.options).to eq(["iburst", "prefer"])
+      ntp.save
+      expect(file.content).to include("server 0.pool.ntp.org iburst prefer\n")
     end
   end
 end
 
 describe CFA::NtpConf::CommandRecord do
-  let(:augeas_element) do
-    tree = CFA::AugeasTree.new
-    tree.add("iburst", nil)
-    value = CFA::AugeasTreeValue.new(tree, "4.pool.ntp.org")
-    { key: "server[]", value: value }
+  let(:ntp) { ntp_conf(file) }
+
+  let(:file) { ntp_file(content) }
+
+  let(:content) do
+    "server 0.pool.ntp.org iburst\n"
   end
 
-  subject(:record) { described_class.new_from_augeas(augeas_element) }
+  before do
+    ntp.load
+  end
+
+  subject(:record) { ntp.records.first }
 
   describe "#options" do
     it "obtains the options of the record" do
@@ -224,19 +290,26 @@ describe CFA::NtpConf::CommandRecord do
     it "sets options to the record" do
       record.options = ["iburst", "prefer"]
       expect(record.options).to eq(["iburst", "prefer"])
+      ntp.save
+      expect(file.content).to include("server 0.pool.ntp.org iburst prefer\n")
     end
   end
 end
 
 describe CFA::NtpConf::FudgeRecord do
-  let(:augeas_element) do
-    tree = CFA::AugeasTree.new
-    tree.add("stratum", "10")
-    value = CFA::AugeasTreeValue.new(tree, "127.127.1.0")
-    { key: "fudge[]", value: value }
+  let(:ntp) { ntp_conf(file) }
+
+  let(:file) { ntp_file(content) }
+
+  let(:content) do
+    "fudge 127.127.1.0 stratum 10\n"
   end
 
-  subject(:record) { described_class.new_from_augeas(augeas_element) }
+  before do
+    ntp.load
+  end
+
+  subject(:record) { ntp.records.first }
 
   describe "#options" do
     it "obtains the options of the record" do
@@ -249,6 +322,8 @@ describe CFA::NtpConf::FudgeRecord do
       options = { "stratum" => "11" }
       record.options = options
       expect(record.options).to eq(options)
+      ntp.save
+      expect(file.content).to include("fudge 127.127.1.0 stratum 11\n")
     end
   end
 
@@ -262,23 +337,30 @@ describe CFA::NtpConf::FudgeRecord do
     it "sets options from a string" do
       record.raw_options = "stratum 11"
       expect(record.options).to eq("stratum" => "11")
+      ntp.save
+      expect(file.content).to include("fudge 127.127.1.0 stratum 11\n")
     end
   end
 end
 
 describe CFA::NtpConf::RestrictRecord do
-  let(:augeas_element) do
-    tree = CFA::AugeasTree.new
-    tree.add("action[]", "default")
-    value = CFA::AugeasTreeValue.new(tree, "127.127.1.0")
-    { key: "restrict[]", value: value }
+  let(:ntp) { ntp_conf(file) }
+
+  let(:file) { ntp_file(content) }
+
+  let(:content) do
+    "restrict -4 default notrap nomodify nopeer\n"
   end
 
-  subject(:record) { described_class.new_from_augeas(augeas_element) }
+  before do
+    ntp.load
+  end
+
+  subject(:record) { ntp.records.first }
 
   describe "#options" do
     it "obtains the options of the record" do
-      expect(record.options).to eq(["default"])
+      expect(record.options).to eq(%w(default notrap nomodify nopeer))
     end
   end
 
@@ -287,12 +369,14 @@ describe CFA::NtpConf::RestrictRecord do
       options = ["default", "notrap"]
       record.options = options
       expect(record.options).to eq(options)
+      ntp.save
+      expect(file.content).to include("restrict -4 default notrap\n")
     end
   end
 
   describe "#raw_options" do
     it "obtains options as string" do
-      expect(record.raw_options).to eq("default")
+      expect(record.raw_options).to eq("default notrap nomodify nopeer")
     end
   end
 
@@ -300,6 +384,8 @@ describe CFA::NtpConf::RestrictRecord do
     it "sets options from a string" do
       record.raw_options = "default notrap"
       expect(record.options).to eq(["default", "notrap"])
+      ntp.save
+      expect(file.content).to include("restrict -4 default notrap\n")
     end
   end
 end
