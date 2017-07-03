b47c5affca869be8b98ca83cf45b052566ce3394
Knut Alejandro Anderssen González <teclator@gmail.com>
2017-07-03 08:41:18 +0100

    Added bug reference.

--- src/modules/NtpClient.rb
+++ src/modules/NtpClient.rb
@@ -598,6 +598,8 @@ module Yast
       # write settings
       return false if !go_next
 
+      # Restrict map records are written first to not mangle the config file
+      # (bsc#983486)
       @ntp_records = restrict_map_records + @ntp_records
 
       log.info "Writing settings #{@ntp_records}"
