cc5c8d2d87356f68c70469e40e60070c9ae42b2f
Josef Reidinger <jreidinger@suse.cz>
2017-11-07 16:13:41 +0100

    add servers widget

--- src/lib/y2ntp_client/widgets.rb
+++ src/lib/y2ntp_client/widgets.rb
@@ -40,6 +40,7 @@ module Y2NtpClient
       current_policy = Yast::NtpClient.ntp_policy
       if !["", "auto"].include?(current_policy)
         items << [current_policy, current_policy]
+      end
     end
 
     def init
@@ -51,6 +52,70 @@ module Y2NtpClient
         log.info "ntp policy modifed to #{value.inspect}"
         Yast::NtpClient.modified = true
         Yast::NtpClient.ntp_policy = value
+      end
+    end
+  end
+
+  class NtpStart < CWM::RadioButtons
+    def initialize
+      textdomain "ntp-client"
+    end
+
+    def label
+      _("Start NTP Daemon")
+    end
+
+    def opt
+      [:notify]
+    end
+
+    def items
+      [
+        # radio button
+        ["never", _("Only &Manually")],
+        # radio button
+        ["sync", _("&Synchronize without Daemon")],
+        # radio button
+        ["boot", _("Now and on &Boot")]
+      ]
+    end
+
+    def init
+      self.value = if Yast::NtpClient.synchronize_time
+        "sync"
+      elsif Yast::NtpClient.run_service
+        "boot"
+      else
+        "never"
+      end
+    end
+
+    def handle
+      # TODO: display interval for sync without daemon
+      nil
+    end
+
+    def store
+      Yast::NtpClient.run_service = value == "boot"
+      Yast::NtpClient.synchronize_time = value == "sync"
+    end
+  end
+
+  class ServersTable < CWM::Table
+    def initialize
+      textdomain "ntp-client"
+    end
+
+    def header
+      [
+        # table header for list of servers
+        _("Synchronization Servers")
+      ]
+    end
+
+    def items
+      Yast::NtpClient.ntp_conf.pools.keys.map do |server|
+        [server, server]
       end
     end
   end
