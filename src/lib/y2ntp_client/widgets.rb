bb54c312b3c477fd82c24dc9d1cd1c1cd59fb98a
Josef Reidinger <jreidinger@suse.cz>
2017-11-07 15:07:16 +0100

    adapt policy widget and drop ShowLog one

--- src/lib/y2ntp_client/widgets.rb
+++ src/lib/y2ntp_client/widgets.rb
@@ -11,89 +11,15 @@ Yast.import "Progress"
 Yast.import "Service"
 
 module Y2NtpClient
-  class ShowLog < CWM::PushButton
-    def initialize
-      textdomain "ntp-client"
-    end
-
-    def handle
-      _("Display &Log...")
-    end
-
-    def handle
-      # TODO: this is good for seeing log, but it is not refreshed with new log lines as it is not
-      # real journalctl content. Maybe some background writting helps?
-      tmp_file = Tempfile.new("yast_chronylog")
-      tmp_file.close
-      begin
-        Yast::SCR.Execute(path(".target.bash"),
-          "/usr/bin/journalctl --boot --unit chronyd --no-pager --no-tail > '#{tmp_file.path}'")
-        Yast::LogView.Display(
-          "file"    => tmp_file.path,
-          "save"    => true,
-          "actions" => [
-            # menubutton entry, try to keep short
-            [
-              _("Restart NTP Daemon"),
-              Yast.fun_ref(method(:restart_daemon), "void ()")
-            ],
-            # menubutton entry, try to keep short
-            [
-              _("Save Settings and Restart NTP Daemon"),
-              Yast.fun_ref(method(:silent_write), "boolean ()")
-            ]
-          ]
-        )
-      ensure
-        tmp_file.unlink
-      end
-      nil
-    end
-
-    def silent_write
-      orig_progress = Yast::Progress.set(false)
-      Yast::NtpClient.Write
-      Yast::Progress.set(orig_progress)
-    end
-
-    def restart_daemon
-      Yast::Service.restart(Yast::NtpClient.service_name)
-    end
-  end
-
-  class CustomPolicy < CWM::InputField
-    def initialize
-      textdomain "ntp-client"
-      @cached_value = Yast::NtpClient.ntp_policy
-    end
-
-    def label
-      _("&Custom Policy")
-    end
-
-    def opt
-      [:notify]
-    end
-
-    def init
-      self.value = @cached_value
-    end
-
-    def handle
-      @cached_value = value
-    end
-  end
-
   class PolicyCombo < CWM::ComboBox
-    # @param replace_point replace point where to show custom policy input box
-    def initialize(replace_point)
+    def initialize
       textdomain "ntp-client"
-      @custom_policy_widget = CustomPolicy.new
-      @replace_point = replace_point
     end
 
     def label
-      _("&Runtime Configuration Policy")
+      # TRANSLATORS: label for widget that allows to define if ntp configiration is only
+      # from its source or dynamically extended e.g. via DHCP
+      _("Configuration Source")
     end
 
     def help
@@ -101,54 +27,30 @@ module Y2NtpClient
     end
 
     def opt
-      [:notify]
+      [:editable]
     end
 
     def items
-      # FIXME: usability
-      [
+      items = [
         # combo box item
-        [:nomodify, _("Manual")],
+        ["", _("Static")],
         # combo box item
-        [:auto, _("Auto")],
-        # combo box item
-        [:custom, _("Custom")]
+        ["auto", _("Dynamic")]
       ]
-    end
-
-    def handle
-      if value == :custom
-        @replace_point.replace(@custom_policy_widget)
-      else
-        @replace_point.replace(CWM::Empty.new("nothing_custom"))
-      end
+      current_policy = Yast::NtpClient.ntp_policy
+      if !["", "auto"].include?(current_policy)
+        items << [current_policy, current_policy]
     end
 
     def init
-      self.value = if Yast::NtpClient.PolicyIsNomodify
-        :nomodify
-      elsif Yast::NtpClient.PolicyIsAuto
-        :auto
-      else
-        :custom
-      end
-      handle
+      self.value = Yast::NtpClient.ntp_policy
     end
 
     def store
-      tmp = Yast::NtpClient.ntp_policy
-
-      Yast::NtpClient.ntp_policy = case value
-      when :nomodify then ""
-      when :auto then "auto"
-      when :custom then @custom_policy_widget.value
-      else
-        raise "unexpected value '#{value}'"
-      end
-
-      if tmp != Yast::NtpClient.ntp_policy
-        log.info "set modified to true"
+      if value != Yast::NtpClient.ntp_policy
+        log.info "ntp policy modifed to #{value.inspect}"
         Yast::NtpClient.modified = true
+        Yast::NtpClient.ntp_policy = value
       end
     end
   end
